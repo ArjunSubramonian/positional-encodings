194c876a4d03c10d7b04c5d6748e62fa6eb26296
root <root@Scai2.CS.UCLA.EDU>
2021-02-02 05:59:19 +0000

    fixed validation script

--- train-graph-transformer-dist.py
+++ train-graph-transformer-dist.py
@@ -4,6 +4,7 @@
 import torch
 import torch.nn as nn
 from torch_geometric.data import DataLoader
+from tensorboardX import SummaryWriter
 import argparse
 import numpy as np
 import random
@@ -18,7 +19,6 @@ import torch.distributed as dist
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='0,2,3,6'
 
 parser = argparse.ArgumentParser(description='PyTorch implementation of relative positional encodings and relation-aware self-attention for graph Transformers')
 args = parser.parse_args("")
@@ -63,9 +63,10 @@ args.relation_type = 'shortest_dist'
 args.pre_transform = compute_mutual_shortest_distances
 args.max_vocab = 12
 args.split = 'scaffold'
-args.num_epochs = 400
+args.num_epochs = 50
 args.weights_dropout = True
-args.grad_acc = 128
+args.grad_acc = 256
+args.writer = SummaryWriter(log_dir='runs/molhiv')
 
 
 # %%
@@ -93,31 +94,27 @@ def train(rank, num_epochs, world_size):
     if args.split == 'scaffold':
         split_idx = dataset.get_idx_split()
         train_sampler = DistributedSampler(
-            dataset[split_idx["train"]], rank=rank, num_replicas=world_size, shuffle=False
-        )
-        test_sampler = DistributedSampler(
-            dataset[split_idx["test"]], rank=rank, num_replicas=world_size, shuffle=False
+            dataset[split_idx["train"]], rank=rank, num_replicas=world_size
         )
         
         train_loader = DataLoader(dataset[split_idx["train"]], batch_size=args.batch_size, shuffle=False, drop_last=True, sampler=train_sampler)
-        test_loader = DataLoader(dataset[split_idx["test"]], batch_size=args.batch_size, shuffle=False, drop_last=True, sampler=test_sampler)
+        if rank == 0:
+            test_loader = DataLoader(dataset[split_idx["test"]], batch_size=args.batch_size, shuffle=False, drop_last=True)
     elif args.split == '80-20':
         train_sampler = DistributedSampler(
-            dataset[:int(0.8 * len(dataset))], rank=rank, num_replicas=world_size, shuffle=False
-        )
-        test_sampler = DistributedSampler(
-            dataset[int(0.8 * len(dataset)):], rank=rank, num_replicas=world_size, shuffle=False
+            dataset[:int(0.8 * len(dataset))], rank=rank, num_replicas=world_size
         )
         
         train_loader = DataLoader(dataset[:int(0.8 * len(dataset))], batch_size=args.batch_size, shuffle=False, drop_last=True, sampler=train_sampler)
-        test_loader = DataLoader(dataset[int(0.8 * len(dataset)):], batch_size=args.batch_size, shuffle=False, drop_last=True, sampler=test_sampler)
+        if rank == 0:
+            test_loader = DataLoader(dataset[int(0.8 * len(dataset)):], batch_size=args.batch_size, shuffle=False, drop_last=True)
 
     model = GraphTransformerModel(args).cuda(rank)
     model = DistributedDataParallel(model, device_ids=[rank])
     batch_device = torch.device('cuda:'+ str(rank) if torch.cuda.is_available() else 'cpu')
     
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 1000, eta_min=1e-6)
+    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 1000, eta_min=1e-6)
     criterion = torch.nn.BCEWithLogitsLoss(reduction = "mean")
     evaluator = Evaluator(name=args.dataset)
 
@@ -125,7 +122,8 @@ def train(rank, num_epochs, world_size):
         ############
         # TRAINING #
         ############
-
+        train_sampler.set_epoch(epoch)
+        
         model.train()
 
         loss_epoch = 0
@@ -138,63 +136,79 @@ def train(rank, num_epochs, world_size):
             y = batch.y.float()
             is_valid = ~torch.isnan(y)
 
-            loss = criterion(z[is_valid], y[is_valid])
+            loss = criterion(z[is_valid], y[is_valid]) # / args.grad_acc 
             loss.backward()
             # gradient accumulation
             if (idx + 1) % args.grad_acc == 0:
                 optimizer.step()
                 optimizer.zero_grad()
-                scheduler.step()
+                # scheduler.step()
 
             loss_epoch += loss.detach().item()
-
+        
         if rank == 0:
             print('Epoch:', epoch + 1)
-            print('Train loss:', loss_epoch / len(train_loader))
+            args.writer.add_scalar("Loss/train", loss_epoch / len(train_loader), epoch + 1)
+        print('Train loss (cuda:' + str(rank) + '):', loss_epoch / len(train_loader))
 
-        ##############
-        # EVALUATION #
-        ##############
-
-        model.eval()
-
-        with torch.no_grad():
-            loss_epoch = 0
-            y_true = []
-            y_scores = []
-            for idx, batch in enumerate(test_loader):
-                batch = batch.to(batch_device, non_blocking=True)
-                z = model(batch)
-
-                y = batch.y.float()
-                y_true.append(y)
-                y_scores.append(z)
-                is_valid = ~torch.isnan(y)
-
-                loss = criterion(z[is_valid], y[is_valid])
-                loss_epoch += loss.detach().item()
-
-            y_true = torch.cat(y_true, dim = 0)
-            y_scores = torch.cat(y_scores, dim = 0)
-
-        input_dict = {"y_true": y_true, "y_pred": y_scores}
-        result_dict = evaluator.eval(input_dict)
         if rank == 0:
+            ##############
+            # EVALUATION #
+            ##############
+
+            model.eval()
+
+            with torch.no_grad():
+                loss_epoch = 0
+                y_true = []
+                y_scores = []
+                for idx, batch in enumerate(test_loader):
+                    batch = batch.to(batch_device, non_blocking=True)
+                    z = model(batch)
+
+                    y = batch.y.float()
+                    y_true.append(y)
+                    y_scores.append(z)
+                    is_valid = ~torch.isnan(y)
+
+                    loss = criterion(z[is_valid], y[is_valid])
+                    loss_epoch += loss.detach().item()
+
+                y_true = torch.cat(y_true, dim = 0)
+                y_scores = torch.cat(y_scores, dim = 0)
+
+            input_dict = {"y_true": y_true, "y_pred": y_scores}
+            result_dict = evaluator.eval(input_dict)
+        
+            args.writer.add_scalar("Loss/test", loss_epoch / len(test_loader), epoch + 1)
             print('Test loss:', loss_epoch / len(test_loader))
             print('Test ROC-AUC:', result_dict[args.eval_metric])
-            
+
             print()
-        
+
             if (epoch + 1) % 10 == 0:
                 torch.save(
                     model.state_dict(),
                     f'./models/model_{epoch + 1}_{args.dataset}.pth'
                 )
         
-WORLD_SIZE = torch.cuda.device_count()
 if __name__=="__main__":
+    os.environ['CUDA_VISIBLE_DEVICES']='2,3,5,6,7'
+    WORLD_SIZE = torch.cuda.device_count()
     mp.spawn(
         train, args=(args.num_epochs, WORLD_SIZE),
         nprocs=WORLD_SIZE, join=True
     )
+    args.writer.close()
 
+
+# %%
+# try 'bace' dataset
+# Longformer, if fully-connected data is too much information
+# random sampling for training dataset
+# plot learning curve
+# warmup steps for learning
+# there might be bugs
+# double-check DistributedSampler
+
+# %%
